31bd9d45bae04da4c813e9e25a0fcf632916c737
Hansaka <hansakajayan@gmail.com>
2026-06-30 21:19:00 +0530

    Remove all project files

--- src/pages/admin/AdminConsole.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import StaticHtmlFrame from './StaticHtmlFrame.jsx'
-import adminConsoleHtml from './index.html?raw'
-
-export default function AdminConsole() {
-  return <StaticHtmlFrame title="Admin Console" html={adminConsoleHtml} />
-}
