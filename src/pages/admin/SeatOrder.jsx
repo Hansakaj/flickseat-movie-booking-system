1219a703791c37c61f2411a6a5f944681769765d
Hansaka <hansakajayan@gmail.com>
2026-07-01 20:02:24 +0530

    Remove all project files for temparory

--- src/pages/admin/SeatOrder.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import StaticHtmlFrame from './StaticHtmlFrame.jsx'
-import seatOrderHtml from './seat-order.html?raw'
-
-export default function SeatOrder() {
-  return <StaticHtmlFrame title="Seat Order" html={seatOrderHtml} />
-}
